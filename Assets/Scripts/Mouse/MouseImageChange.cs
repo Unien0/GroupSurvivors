9134e38bdfc092f471595304ef51f7ae6dee78d5
Unien <unien7415@gmail.com>
2023-09-06 15:53:23 +0900

    添加鼠标虚拟图片

---
+++ Assets/Scripts/Mouse/MouseImageChange.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MouseImageChange : MonoBehaviour
+{
+    public Sprite mouseTexture,airTexture;
+    private Sprite currentSprite;
+    private Image cursorImage;
+    private RectTransform cursorCanvas;
+
+    private void Start()
+    {
+        cursorCanvas = GameObject.FindGameObjectWithTag("CursorCanvas").GetComponent<RectTransform>();
+        cursorImage = cursorCanvas.GetChild(0).GetComponent<Image>();
+        currentSprite = mouseTexture;
+        SetCursorImage(mouseTexture);
+    }
+
+    private void Update()
+    {
+        if (cursorCanvas == null) return;
+
+        cursorImage.transform.position = Input.mousePosition;
+
+        if (!InteractWithUI())
+        {
+            SetCursorImage(currentSprite);
+        }
+        else
+        {
+            SetCursorImage(mouseTexture);
+        }
+    }
+
+    /// <summary>
+    /// 设置鼠标图片
+    /// </summary>
+    /// <param name="sprite"></param>
+    private void SetCursorImage(Sprite sprite)
+    {
+        cursorImage.sprite = sprite;
+        cursorImage.color = new Color(1, 1, 1, 1);
+    }
+
+    /// <summary>
+    /// 是否与UI互动
+    /// </summary>
+    /// <returns></returns>
+    private bool InteractWithUI()
+    {
+        if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+        {
+            return true;
+        }
+        return false;
+    }
+}
