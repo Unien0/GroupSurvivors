b0d56f7d6911218a7683099f3624bbe50a91ac3f
DESKTOP-DII4QQ9\24105 <unien7415@gmail.com>
2023-09-17 16:31:31 +0900

    设置道具的预制体，新增武器柯基犬

---
+++ Assets/Scripts/Weapons/Weapon Behaviour/LuckyDogBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuckyDogBehaviour : ProjectileWeaponBehaviour
+{
+    private Transform targetEnemy;
+    protected override void Start()
+    {
+        base.Start();
+        FindNearestEnemy();
+    }
+
+    void Update()
+    {
+        if (targetEnemy != null)
+        {
+            Vector3 moveDirection = (targetEnemy.position - transform.position).normalized;
+            transform.Translate(moveDirection * currentSpeed * Time.deltaTime);
+        }
+        else
+        {
+            FindNearestEnemy();
+        }
+    }
+    void FindNearestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); // 根据需要修改标签
+
+        float closestDistance = Mathf.Infinity;
+        targetEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < closestDistance)
+            {
+                closestDistance = distanceToEnemy;
+                targetEnemy = enemy.transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/Weapon Controllers/LuckyDogController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuckyDogController : WeaponController
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        GameObject spawnedKnife = Instantiate(weaponData.Prefab);//生成投掷物
+        spawnedKnife.transform.position = transform.position; //将位置指定为与该玩家的父对象相同
+        //spawnedKnife.GetComponent<KnifeBehaviour>().DirectionChecker(pm.lastMovedVector);   //参考并设置方向
+    }
+}
