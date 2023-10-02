255a3582b480a09ab0b45c4585556c3f15e3f002
UNIEN0 <unien7415@gmail.com>
2023-10-02 12:11:33 +0900

    -0.5版

--- Assets/Scripts/Title/SceneController.cs
+++ Assets/Scripts/Title/SceneController.cs
@@ -10,4 +10,9 @@ public class SceneController : MonoBehaviour
         SceneManager.LoadScene(name);
         Time.timeScale = 1;
     }
+
+    public void GameOver()
+    {
+        Application.Quit();
+    }
 }
